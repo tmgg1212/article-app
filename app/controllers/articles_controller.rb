fe3fd421bab0f42f12413c2d8ee67bf4d5d549b2
tmgg1212 <75664927+tmgg1212@users.noreply.github.com>
2021-02-04 12:27:53 +0900

    README

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :show, :update, :destroy]
+
   def index
     @articles = Article.all.order(created_at: :desc)
   end
@@ -20,10 +22,16 @@ class ArticlesController < ApplicationController
   def show
   end
 
+  
+
   private
 
   def article_params
     params.require(:article).permit(:title, :text, :category_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
